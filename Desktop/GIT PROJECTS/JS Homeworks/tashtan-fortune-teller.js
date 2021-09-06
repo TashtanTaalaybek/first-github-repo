0d4651a1f2f61100303bce5e96c00d2fbc08871c
TashtanTaalaybek <tashtand@gmail.com>
2021-09-06 17:52:50 -0400

    Tashtan's fortune teller HW

---
+++ Desktop/GIT PROJECTS/JS Homeworks/tashtan-fortune-teller.js
@@ -0,0 +1,6 @@
+let numOfChildren = 3;
+let partnersName = 'Aika';
+let geoLocation = 'New York';
+let jobtitle = 'Software Engineer';
+
+console.log('You will be a ' + jobtitle + ' in ' + geoLocation + ', and married to ' +  partnersName + ' with ' + numOfChildren + ' kids ')
